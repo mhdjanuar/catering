0f9e27cfe4f18719dc70ead483e1703c612f16dd
mhdjauar <mhdjanuar.17@gmail.com>
2025-01-30 17:21:14 +0700

    fix: use db mysql free

--- src/app/utils/db.js
+++ src/app/utils/db.js
@@ -2,8 +2,8 @@ import mysql from "mysql2/promise";
 
 // Buat koneksi ke database
 export const db = mysql.createPool({
-  host: "localhost", // Host database
-  user: "root", // Username
-  password: "", // Password
-  database: "catering", // Nama database
+  host: "sql12.freesqldatabase.com", // Host database
+  user: "sql12760294", // Username
+  password: "7XBnUEBHwp", // Password
+  database: "sql12760294", // Nama database
 });
